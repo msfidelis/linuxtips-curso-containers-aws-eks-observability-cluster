02e1d044f6329b9d624b9ef0cff1d0bd276c754d
Matheus Fidelis <matheus@nanoshots.com.br>
2025-04-23 22:25:51 -0300

    Aula Tempo

--- route53.tf
+++ route53.tf
@@ -14,3 +14,11 @@ resource "aws_route53_record" "loki" {
   ttl     = "30"
   records = [aws_lb.loki.dns_name]
 }
+
+resource "aws_route53_record" "tempo" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = format("tempo.%s.local",  var.project_name)
+  type    = "CNAME"
+  ttl     = "30"
+  records = [aws_lb.tempo.dns_name]
+}
